83c10c5eea15c25bdf996dba6ece562b697c0add
Pedro Côrte-Real <pedro@pedrocr.net>
2010-03-09 15:58:53 -0800

    Remove temporary directories when comparison is done.

--- lib/comparator.rb
+++ lib/comparator.rb
@@ -26,6 +26,7 @@ class Comparator
       run_cmd_size_of_dir("To:", pkg2, dir2, resultfile)
       Util.run_cmd("echo -n \"Differences: \" >> #{resultfile}")
       Util.run_cmd("diff -uNr #{dir1} #{dir2} | diffstat -b -f 0 | tail -n 1 >> #{resultfile}")
+      FileUtils.rm_rf [dir1, dir2]
     end
   end
 
