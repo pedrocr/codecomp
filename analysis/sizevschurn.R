8172bf12435b2681647f0bf5e4f42d291a31f4aa
Pedro Côrte-Real <pedro@pedrocr.net>
2011-05-06 12:38:11 -0700

    Simplify R code for regression lines

--- analysis/sizevschurn.R
+++ analysis/sizevschurn.R
@@ -3,10 +3,10 @@ pdf(file="generated/sizevschurnplot.pdf")
 
 reg1 <- glm(log(CHURN) ~ log(SIZE))
 plot(log(SIZE),log(CHURN),cex=0.7)
-abline(reg1$coeff[1],reg1$coeff[2])
+abline(reg1)
 
 reg2 <- glm(CHURN ~ log(SIZE))
 plot(log(SIZE),CHURN,cex=0.7)
-abline(reg2$coeff[1],reg2$coeff[2])
+abline(reg2)
 
 print(summary(reg2))
