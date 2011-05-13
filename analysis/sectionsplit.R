1bdaf5f85cde83e07cbf1b7a2f290e195d99d669
Pedro Côrte-Real <pedro@pedrocr.net>
2011-05-13 01:58:41 -0700

    Change graph label

--- analysis/sectionsplit.R
+++ analysis/sectionsplit.R
@@ -13,12 +13,12 @@ title(xlab= "Release cycle")
 
 ymarks = c(-100,10,40,70,100,130,160,2000)
 axis(2, las=1, at=ymarks*10^6, labels=ymarks)
-title(ylab="Total Churn\n(Millions of Diff Changed Lines)")
+title(ylab="Total Changes\n(Millions of Diff Changed Lines)")
 
 lines(CORE, type="o", col="red")
 lines(BASE, type="o", col="blue")
 lines(USER, type="o", col="green")
 lines(total, type="o", col="black",lty=2)
 
-legend('topright', c("total","base","user","core"), 
-   col=c("black","blue","green","red"), pch=21, lty=c(2,1,1,1), bty="n");
+legend('topright', c("total","user","base","core"), 
+   col=c("black","green","blue","red"), pch=21, lty=c(2,1,1,1), bty="n");
