42329a95558ec1a07817bd83008065fa7ed79261
Pedro Côrte-Real <pedro@pedrocr.net>
2011-05-20 02:01:46 -0700

    Run diffstat -m to estimate modifications

--- lib/comparator.rb
+++ lib/comparator.rb
@@ -24,8 +24,13 @@ class Comparator
       file2.download(dir2 = tmpdir+"/"+sources2.distro)
       run_cmd_size_of_dir("From:", pkg1, dir1, resultfile)
       run_cmd_size_of_dir("To:", pkg2, dir2, resultfile)
-      Util.run_cmd("echo -n \"Differences: \" >> #{resultfile}")
-      Util.run_cmd("diff -uNr #{dir1} #{dir2} | diffstat -b -f 0 | tail -n 1 >> #{resultfile}")
+      # Diff exits with code 1 on differences hence the || [... bit
+      Util.run_cmd "diff -uNr #{dir1} #{dir2} > #{resultfile}.diff || [ \"$?\" -eq 1 ]"
+      Util.run_cmd "echo -n \"diffstat: \" >> #{resultfile}"
+      Util.run_cmd "cat #{resultfile}.diff | diffstat -b -f 0 | tail -n 1 >> #{resultfile}"
+      Util.run_cmd "echo -n \"diffstat -m: \" >> #{resultfile}"
+      Util.run_cmd "cat #{resultfile}.diff | diffstat -m -b -f 0 | tail -n 1 >> #{resultfile}"
+      FileUtils.rm resultfile+".diff"
       FileUtils.rm_rf tmpdir
     end
   end
